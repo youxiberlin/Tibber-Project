207205bbfb5954467e4d08cc36c6c1f77e84a614
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-15 10:45:03 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { getName } from '../lib/api'
+
+
+function App({ allPosts }) {
+	const data = allPosts;
+	console.log('data', data)
+  return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+	const allPosts = (await getName()) || []
+	return {
+	  props: { allPosts },
+	}
+  }
+
+export default App
