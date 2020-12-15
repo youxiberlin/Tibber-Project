d2cd7e9b7491b539eaf6be89eee2051df076e4d2
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-15 11:02:56 +0100

    Add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { getName } from '../lib/api'
-
+import React from "react"
+import { render } from "react-dom"
+import { getName } from "../lib/api"
 
 function App({ allPosts }) {
-	const data = allPosts;
-	console.log('data', data)
+  const data = allPosts
+  console.log("data", data)
   return (
-      <div>
-        <h2>My first Apollo app 🚀</h2>
-      </div>
-  );
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  )
 }
 
 export async function getStaticProps({ preview = false }) {
-	const allPosts = (await getName()) || []
-	return {
-	  props: { allPosts },
-	}
+  const allPosts = (await getName()) || []
+  return {
+    props: { allPosts },
   }
+}
 
 export default App
