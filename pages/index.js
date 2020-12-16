9716baf015ea9560ef4bc9d62d4efa1465c99a22
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-16 10:32:35 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
 import React from "react"
+import moment from 'moment';
+moment.locale("en")
 import { render } from "react-dom"
-import { getName, getCurrentSub } from "../lib/api"
+import { useTheme, ThemeProvider, withTheme } from '@emotion/react'
+import theme from '@rebass/preset'
+import {
+    Box,
+    Button,
+    Card,
+    Image,
+    Heading,
+    Text
+} from 'rebass'
+import { getName, getCurrentSub, getTmrSub, calcPrice } from "../lib/api"
 
-function App({ username, currentSub }) {
+function App({ username, currentSub, tmrSub }) {
   return (
-    <div>
-      <h2>Tibber Project 🚀</h2>
-      <h3>username: {username}</h3>
-      {currentSub.map(v => {
-          return (
-              <div>
-                  <div>current</div>
-                  <div>Energy: {v.currentSubscription.priceInfo.current.energy}</div>
-                  <div>StartsAt: {v.currentSubscription.priceInfo.current.startsAt}</div>
-                  <div>Tax: {v.currentSubscription.priceInfo.current.tax}</div>
-                  <div>Total: {v.currentSubscription.priceInfo.current.total}</div>
-              </div>
-        )
-      })}
-    </div>
+    <ThemeProvider theme={theme}>
+        <h2>Tibber Project 🚀</h2>
+        <h3>username: {username}</h3>
+        <Button color="blue" onClick={() => calcPrice({
+            // startAt:Date.now(),
+            mins: 60
+        })}>Wash now</Button>
+    </ThemeProvider>
   )
 }
 
 export async function getStaticProps({ preview = false }) {
   const username = (await getName()) || []
   const currentSub = (await getCurrentSub()) || []
+  const tmrSub = (await getTmrSub()) || []
   return {
-    props: { username, currentSub },
+    props: { username, currentSub, tmrSub },
   }
 }
 
