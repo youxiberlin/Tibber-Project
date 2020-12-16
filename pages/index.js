3a1d3effbc8b8c8a5b9ecf066a2760bb2f28773e
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-16 11:57:18 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React, { useState } from 'react';
 import moment from 'moment';
-moment.locale("en")
-import { render } from "react-dom"
-import { useTheme, ThemeProvider, withTheme } from '@emotion/react'
-import theme from '@rebass/preset'
+moment.locale("en");
+import { render } from "react-dom";
+import { useTheme, ThemeProvider, withTheme } from '@emotion/react';
+import theme from '@rebass/preset';
 import {
     Box,
     Button,
@@ -12,27 +12,51 @@ import {
     Heading,
     Text
 } from 'rebass'
-import { getName, getCurrentSub, getTmrSub, calcPrice } from "../lib/api"
+import { getName, getSub } from "../lib/api"
 
-function App({ username, currentSub, tmrSub }) {
+const calcPrice = async ( {
+    startAt = Date.now(),
+    sub,
+    mins,
+    temp,
+  } ) => {
+
+  const currHour = moment(startAt).hour()
+  const curr = sub.today[currHour - 1]
+  const currPrice = curr.total * ( mins / 60 )
+  console.log('curr', curr)
+  console.log('price', currPrice)
+
+  console.log(sub)
+
+  return currPrice;
+}
+
+const App = ({ username, sub }) => {
+  const [currPrice, setCurrPrice] = useState(null);
+  console.log('price in app', currPrice)
   return (
     <ThemeProvider theme={theme}>
         <h2>Tibber Project 🚀</h2>
         <h3>username: {username}</h3>
-        <Button color="blue" onClick={() => calcPrice({
-            // startAt:Date.now(),
-            mins: 60
-        })}>Wash now</Button>
+        <Button color="blue" onClick={async () => {
+          const price = await calcPrice({
+              sub,
+              mins: 90,
+              temp: 60,
+          });
+          setCurrPrice(price);
+        }}>Wash now</Button>
+        <h3>Current Price: {currPrice} NOK</h3>
     </ThemeProvider>
   )
 }
 
 export async function getStaticProps({ preview = false }) {
   const username = (await getName()) || []
-  const currentSub = (await getCurrentSub()) || []
-  const tmrSub = (await getTmrSub()) || []
+  const sub = (await getSub()) || []
   return {
-    props: { username, currentSub, tmrSub },
+    props: { username, sub },
   }
 }
 
